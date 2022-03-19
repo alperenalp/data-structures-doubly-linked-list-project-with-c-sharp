c882829c0228c8d0eb788deb163c738777c5380e
alperenalp <alperen.alp045@gmail.com>
2022-03-19 13:25:40 +0300

    project

---
+++ my_doubly_linked_list_app/Odev2_cift_yonlu_Link_List/Form1.Designer.cs
@@ -0,0 +1,195 @@
+﻿namespace Odev2_cift_yonlu_Link_List
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(222, 231);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(99, 37);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Listeyi Yazdır";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.listeyiYazdir_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(12, 274);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(524, 96);
+            this.richTextBox1.TabIndex = 1;
+            this.richTextBox1.Text = "";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 68);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(104, 20);
+            this.textBox1.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 52);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(44, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Numara";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(12, 124);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(99, 37);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Başa Ekle";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.BasaEkle_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(12, 167);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(99, 37);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "Sona Ekle";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.sonaEkle_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(222, 124);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(99, 37);
+            this.button4.TabIndex = 6;
+            this.button4.Text = "Araya Ekle";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.arayaEkle_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(222, 167);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(99, 37);
+            this.button5.TabIndex = 7;
+            this.button5.Text = "Aradan Sil";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.aradanSil_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(117, 124);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(99, 37);
+            this.button6.TabIndex = 8;
+            this.button6.Text = "Baştan Sil";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.bastanSil_Click);
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(117, 167);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(99, 37);
+            this.button7.TabIndex = 9;
+            this.button7.Text = "Sondan Sil";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.sondanSil_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 13);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(25, 13);
+            this.label2.TabIndex = 11;
+            this.label2.Text = "İsim";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(12, 29);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(104, 20);
+            this.textBox2.TabIndex = 10;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(702, 482);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox2;
+    }
+}
+
---
+++ my_doubly_linked_list_app/Odev2_cift_yonlu_Link_List/Form1.cs
@@ -0,0 +1,275 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Odev2_cift_yonlu_Link_List
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public class Dugum {
+            public int numara;
+            public string isim;
+            public Dugum sonraki;
+            public Dugum onceki;
+        }
+        Dugum ilk = null;
+        Dugum son = null;
+        private void listeyiYazdir_Click(object sender, EventArgs e)
+        {
+            if (ilk==null)
+            {
+                richTextBox1.Text = "null";
+            }
+            else
+            {
+                richTextBox1.Text = null;
+                Dugum isaretci = ilk;
+                richTextBox1.Text = "null <--- ";
+                while (isaretci != null)
+                {
+                    richTextBox1.Text += Convert.ToString(isaretci.isim + " : " + isaretci.numara + " ---> ");
+                    isaretci = isaretci.sonraki;
+                }
+                richTextBox1.Text += " null";
+            }
+        }
+
+        private void BasaEkle_Click(object sender, EventArgs e)
+        {
+            Dugum yeni = new Dugum();
+            yeni.numara = Convert.ToInt32(textBox1.Text);
+            yeni.isim = textBox2.Text;
+            if (ilk==null)  // liste boşsa
+            {
+                ilk = yeni;
+                son = yeni;
+            }
+            else
+            {
+                ilk.onceki = yeni;
+                yeni.sonraki = ilk;
+                ilk = yeni;
+                ilk.onceki = null;
+            }
+        }
+
+        private void sonaEkle_Click(object sender, EventArgs e)
+        {
+            Dugum yeni = new Dugum();
+            yeni.numara = Convert.ToInt32(textBox1.Text);
+            yeni.isim = textBox2.Text;
+            if (ilk==null)
+            {
+                ilk = yeni;
+                son = yeni;
+            }
+            else
+            {
+                son.sonraki = yeni;
+                yeni.onceki = son;
+                son = yeni;
+                son.sonraki = null;
+            }
+        }
+
+        private void arayaEkle_Click(object sender, EventArgs e)
+        {
+            Dugum yeni = new Dugum();
+            yeni.numara = Convert.ToInt32(textBox1.Text);
+            yeni.isim = textBox2.Text;
+            if (ilk==null)
+            {
+                ilk = yeni;
+                son = yeni;
+            }
+            else if (ilk==son) // tek eleman varsa
+            {
+                Dugum isaretci = ilk;
+                while (isaretci != null)
+                {
+                    if (yeni.numara > isaretci.numara) // tek eleman oldugu icin sona ekleme yapar
+                    {
+                        son.sonraki = yeni;
+                        yeni.onceki = son;
+                        son = yeni;
+                        son.sonraki = null;
+                        break;
+                    }
+                    if(yeni.numara==isaretci.numara)
+                    {
+                        richTextBox1.Text = "Eklemek istediğiniz numara mevcut";
+                        break;
+                    }
+                    isaretci = isaretci.sonraki;
+                }
+
+
+            }
+            else // ikiden fazla eleman varsa
+            {
+                Dugum isaretci1 = null;
+                Dugum isaretci2 = ilk;
+                while (isaretci2!=null)
+                {
+                    if (yeni.numara < isaretci2.numara)  // araya ve başa ekler
+                    {
+                        if (isaretci2==ilk)
+                        {
+                            ilk.onceki = yeni;
+                            yeni.sonraki = ilk;
+                            ilk = yeni;
+                            ilk.onceki = null;
+                        }
+                        else
+                        {
+                            isaretci1.sonraki = yeni;
+                            yeni.onceki = isaretci1;
+                            isaretci2.onceki = yeni;
+                            yeni.sonraki = isaretci2;
+                        }
+                        break;
+                    }
+                    if (yeni.numara > isaretci2.numara && isaretci2.sonraki==null)  // sona ekler
+                    {
+                        son.sonraki = yeni;
+                        yeni.onceki = son;
+                        son = yeni;
+                        son.sonraki = null;
+                        break;
+                    }
+                    if (yeni.numara == isaretci2.numara)
+                    {
+                        richTextBox1.Text = "Eklemek istediğiniz numara mevcut";
+                        break;
+                    }
+                    isaretci1 = isaretci2;
+                    isaretci2 = isaretci2.sonraki;
+                }
+            }
+        }
+
+        private void bastanSil_Click(object sender, EventArgs e)
+        {
+            if (ilk==son)  // tek eleman varsa
+            {
+                ilk = null;
+                son = null;
+            }
+            else
+            {
+                ilk = ilk.sonraki;
+                ilk.onceki = null;
+            }
+        }
+
+        private void sondanSil_Click(object sender, EventArgs e)
+        {
+            if (ilk == son)
+            {
+                ilk = null;
+                son = null;
+            }
+            else
+            {
+                son = son.onceki;
+                son.sonraki = null;
+            }
+        }
+
+        private void aradanSil_Click(object sender, EventArgs e)
+        {
+            
+            int silincekNo = Convert.ToInt32(textBox1.Text);
+            int Kontrol = 0;
+            if (ilk.numara == silincekNo)
+            {
+                ilk = ilk.sonraki;
+            }
+            else
+            {
+                Dugum isaretci1 = null; // bir onceki dugumu bulmak icin gerekli
+                Dugum isaretci2 = ilk;
+                Dugum isaretci3 = ilk.sonraki;  // bir sonraki dugumu bulmak icin gerekli
+                while (isaretci2!=null)
+                {
+                    if (isaretci2.numara==silincekNo && isaretci2.sonraki !=null)  //sondakini silemez
+                    {
+                        isaretci1.sonraki = isaretci2.sonraki;
+                        isaretci2.sonraki.onceki = isaretci1;
+                    }
+                    if (isaretci2.numara==silincekNo && isaretci2.sonraki==null)  //sondakini siler
+                    {
+                        son = son.onceki;
+                        son.sonraki = null;
+                    }
+
+                    isaretci1 = isaretci2;
+                    isaretci2 = isaretci2.sonraki;
+                }
+            }
+            
+        }
+    }
+}
+
+/*
+ * 
+ * while (isaretci2 != null)  // Silinecek Numaranın adedini kontrol eder
+            {
+                if (isaretci2.numara == silincekNo)
+                {
+                    Kontrol++;
+                }
+                isaretci2 = isaretci2.sonraki;
+            }
+            for (int i = 0; i < Kontrol; i++)  // for silinecek numara kadar donmesini saglar
+            {
+                isaretci1 = null;
+                isaretci2 = ilk;
+                isaretci3 = ilk.sonraki;
+                while (isaretci2 != null)
+                {
+                    if (ilk == son)
+                    {
+                        ilk = null;
+                        son = null;
+                        break;
+                    }
+                    else
+                    {
+                        if (silincekNo == isaretci2.numara && isaretci2 == ilk)  // baştan siler
+                        {
+                            ilk = ilk.sonraki;
+                            ilk.onceki = null;
+                            break;
+                        }
+                        if (silincekNo == isaretci2.numara && isaretci2 == son)  // sondan siler
+                        {
+                            son = son.onceki;
+                            son.sonraki = null;
+                            break;
+                        }
+                        if (silincekNo == isaretci2.numara && isaretci2 != ilk && isaretci2 != son)  // aradan siler
+                        {
+                            isaretci1.sonraki = isaretci3;
+                            isaretci3.onceki = isaretci1;
+                            break;
+                        }
+                    }
+                    isaretci1 = isaretci2;
+                    isaretci2 = isaretci2.sonraki;
+                    isaretci3 = isaretci3.sonraki;
+                }
+            }
+*/
